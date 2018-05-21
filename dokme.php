9978ff77f51658f7987333af116800290a87de30
AliRezaMohammadiDoost <alireza.mohammadi.1990@gmail.com>
2018-05-21 22:40:06 +0430

    code formated

--- dokme.php
+++ dokme.php
@@ -9,24 +9,24 @@
  * License: GPL
  * Text Domain: Dokme
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	die;
+if (!defined('ABSPATH')) {
+    die;
 }
 
 // path plugin
-define( 'DOKME_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define('DOKME_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 // hook
-register_activation_hook( __FILE__, array( 'Dokme', 'plugin_activation' ) );
-register_deactivation_hook( __FILE__, array( 'Dokme', 'plugin_deactivation' ) );
+register_activation_hook(__FILE__, array('Dokme', 'plugin_activation'));
+register_deactivation_hook(__FILE__, array('Dokme', 'plugin_deactivation'));
 
 // class dokme
 global $dokme;
-require_once( DOKME_PLUGIN_DIR . 'class-dokme.php' );
-$dokme = new Dokme( __FILE__ );
+require_once(DOKME_PLUGIN_DIR . 'class-dokme.php');
+$dokme = new Dokme(__FILE__);
 
 // Ajax Request
-add_action( 'wp_ajax_updateToken', array( 'Dokme', 'updateToken' ) );
-add_action( 'wp_ajax_syncAllCategories', array( 'Dokme', 'syncAllCategories' ) );
-add_action( 'wp_ajax_syncAllProducts', array( 'Dokme', 'syncAllProducts' ) );
-add_action( 'wp_ajax_selectedCategories', array( 'Dokme', 'selectedCategories' ) );
+add_action('wp_ajax_updateToken', array('Dokme', 'updateToken'));
+add_action('wp_ajax_syncAllCategories', array('Dokme', 'syncAllCategories'));
+add_action('wp_ajax_syncAllProducts', array('Dokme', 'syncAllProducts'));
+add_action('wp_ajax_selectedCategories', array('Dokme', 'selectedCategories'));
