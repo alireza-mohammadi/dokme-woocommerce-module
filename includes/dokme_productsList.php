8c33f06ad2c864ff990eaf100300122210601d4b
AliRezaMohammadiDoost <alireza.mohammadi.1990@gmail.com>
2018-10-08 11:22:57 +0330

    fix product list

--- includes/dokme_productsList.php
+++ includes/dokme_productsList.php
@@ -5,7 +5,6 @@ require_once 'dokme_sendRequest.php';
 
 class Dokme_ProductsList extends WP_List_Table
 {
-
     function __construct()
     {
         parent::__construct(
@@ -20,10 +19,8 @@ class Dokme_ProductsList extends WP_List_Table
     function column_default($item, $column_name)
     {
         switch ($column_name) {
-            case 'id':
             case 'product_id':
-            case 'status':
-            case 'updated_at':
+            case 'name':
                 return $item[$column_name];
             default:
                 return print_r($item, true);
@@ -40,18 +37,6 @@ class Dokme_ProductsList extends WP_List_Table
         return sprintf('%1$s <span style="color:silver">(id:%2$s)</span>%3$s', $item['title'], $item['ID'], $this->row_actions($actions));
     }
 
-    function column_product_name($item)
-    {
-        /*
-        $actions = array(
-            'send' => sprintf('<a href="?page=%s&paged=%s&action=%s&product=%s">ارسال</a>', $_REQUEST['page'], '', 'send', $item['product_id']),
-            'delete' => sprintf('<a href="?page=%s&paged=%s&action=%s&product=%s">حذف</a>', $_REQUEST['page'], '', 'delete', $item['product_id'])
-        );
-
-        return sprintf('%1$s %2$s', $item['product_name'], $this->row_actions($actions));
-        */
-    }
-
     function column_cb($item)
     {
         return sprintf('<input type="checkbox" name="%1$s[]" value="%2$s" />', $this->_args['singular'], $item['product_id']);
@@ -65,11 +50,8 @@ class Dokme_ProductsList extends WP_List_Table
     {
         return array(
             'cb' => '<input type="checkbox" />',
-            'id' => 'ایدی',
-            'product_id' => 'شماره کالا',
-            'product_name' => 'نام کالا',
-            //'status' => 'وضعیت ارسال',
-            //'updated_at' => 'تاریخ به‌روز‌رسانی'
+            'product_id' => __('ایدی کالا'),
+            'name' => __('نام محصول')
         );
     }
 
@@ -80,50 +62,10 @@ class Dokme_ProductsList extends WP_List_Table
     function get_sortable_columns()
     {
         return array(
-            'id' => array('id', true),
-            'status' => array('status', true),
-            'updated_at' => array('updated_at', false)
+            'id' => array(
+                'id', true
+            )
         );
-    }
-
-    function get_bulk_actions()
-    {
-        /*
-        $actions = array(
-            'synchronize' => 'ارسال کالاها',
-            'delete' => 'حذف کالاها',
-        );
-        return $actions;
-        */
-    }
-
-    function process_bulk_action()
-    {
-        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-            return;
-        }
-
-        if (!isset($_GET['product'])) {
-            return;
-        }
-
-        $productId = $_GET['product'];
-
-        if ($this->current_action() === 'send') {
-            $sendRequest = new Dokme_SendRequest();
-            $result = $sendRequest->syncProduct(array($productId));
-
-            if ($result['status']) {
-                Dokme_Dbsync::update($productId);
-            }
-        }
-
-        if ($this->current_action() === 'delete') {
-            $sendRequest = new Dokme_SendRequest();
-            $sendRequest->deleteProducts($productId);
-
-            Dokme_Dbsync::delete($productId);
-        }
     }
 
     /**
@@ -134,17 +76,13 @@ class Dokme_ProductsList extends WP_List_Table
      *
      * @return mixed
      */
-    public static function get_syncd($per_page, $page_number)
+    public static function get_lists($per_page, $page_number)
     {
         global $wpdb;
 
-        $query = "SELECT SQL_CALC_FOUND_ROWS a.* , pl.`post_title` as product_name FROM {$wpdb->prefix}dokme_sync a LEFT JOIN {$wpdb->prefix}posts pl ON (pl.`id` = a.`product_id`)";
+        $tp = $wpdb->prefix . 'posts';
 
-        if (!empty($_REQUEST['orderby'])) {
-            $query .= ' ORDER BY ' . esc_sql($_REQUEST['orderby']);
-            $query .= $_REQUEST['order'] ? ' ' . esc_sql($_REQUEST['order']) : ' ASC';
-        }
-
+        $query = "SELECT `ID` AS `product_id` ,`post_title` AS `name` FROM " . $tp . " WHERE " . $tp . ".`post_type` = 'product' AND " . $tp . ".`post_status` = 'publish'";
         $query .= " LIMIT $per_page OFFSET " . ($page_number - 1) * $per_page;
 
         return $wpdb->get_results($query, 'ARRAY_A');
@@ -158,28 +96,22 @@ class Dokme_ProductsList extends WP_List_Table
     public static function record_count()
     {
         global $wpdb;
-
-        $query = "SELECT COUNT(*) FROM {$wpdb->prefix}dokme_sync";
-
-        return $wpdb->get_var($query);
+        return $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE `post_type` = 'product' AND `post_status` = 'publish'");
     }
 
     function prepare_items()
     {
         $per_page = 50;
 
-        //
-        $hidden = array();
-        $columns = $this->get_columns();
-        $sortable = $this->get_sortable_columns();
-        $this->_column_headers = array($columns, $hidden, $sortable);
-
-        //
-        $this->process_bulk_action();
+        $this->_column_headers = array(
+            $this->get_columns(),
+            array(),
+            $this->get_sortable_columns()
+        );
 
         //
         $current_page = $this->get_pagenum();
-        $this->items = $this->get_syncd($per_page, $current_page);
+        $this->items = $this->get_lists($per_page, $current_page);
 
         //
         $total_items = $this->record_count();
